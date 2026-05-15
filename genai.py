8cfeb25b08ba5f989b8c0907412f3289924b21eb
zweyaungnitun <zweyaungnitun2020@gmail.com>
2026-05-15 15:26:08 +0630

    vector indexing

--- genai.py
+++
@@ -1,6 +0,0 @@
-import google.genai as genai
-client = genai.Client(api_key="AIzaSyBqir0zCnZc8A0rLFNrYfFhi0xEr3K5P2I")
-for m in client.models.list():
-    # Use supported_actions instead of supported_methods
-    if 'embedContent' in m.supported_actions:
-        print(f"Model Name: {m.name}")
